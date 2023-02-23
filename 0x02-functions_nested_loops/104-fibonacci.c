8216e03c6f491947e725e47a9280af256b624e23
M.Ouklilene <122834424+m-ouklilene@users.noreply.github.com>
2023-02-23 02:54:27 -0800

    love

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 
 /**
- * main - prints fibonnaci numbers
- *
- * Description: prints from 1 to 98
- * Return: Always(0) Success
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
+ * Return: 0
  */
 
 int main(void)
 {
-	int num1, num2, i, n;
-	int next_num;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	n = 98;
+	printf("1");
 
-	for (i = 1; i <= n; i++)
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
-		if (i == 98)
-		{
-			printf("%d, ", num2);
-		}
-		else
-		{
-			printf("%d, ", num2);
-			next_num = num1 + num2;
-			num1 = num2;
-			num2 = next_num;
-		}
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
 	}
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
 	printf("\n");
 
 	return (0);
